34a63dd89ff8c5cdf0c4fa1dbbcb107e465f5053
saman zahid <samanzahid22@gmail.com>
2018-12-12 16:12:01 +0100

    updated report

--- lab5.R
+++ lab5.R
@@ -31,8 +31,24 @@ quality
 # a - Cross validation: Partition the data into equally sized subsets and perform model assesment.
 ## Rosetta performs 10 fold cross validation by default
 
+nrow(autconDefault$main)
 ## Question 4
 
+paste("Total number of rules : " ,nrow(rules))
+paste("3 Most significant rules are:")
+rules[(1:3),]
+
+ind <- rules$PVAL < 0.05
+sig_rules <- rules$DECISION[ind]
+class_control <- length(which(sig_rules == "control"))
+class_autism <- length(which(sig_rules == "autism"))
+
+paste("Number of significant rules for class control : " ,class_control)
+
+paste("Number of significant rules for class autism : " ,class_autism)
+
 saveLineByLine(rules = rules, "rules.txt")
+
+
 
 ## Question 5
